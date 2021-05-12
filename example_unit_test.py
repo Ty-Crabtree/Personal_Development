a9a9fe93f428f5992470aa62411768b11d58e62f
Ty Crabtree <crabtree.tyler@gmail.com>
2021-05-11 17:37:17 -0700

    Follow lint standards :)

--- example_unit_test.py
+++ example_unit_test.py
@@ -1,15 +1,27 @@
-# This is a sample Python script.
 import pytest
+
 
 @pytest.mark.parametrize('name', ["Ty", "Max"])
 def test_print_hi(name):
-    print(name)
+    """
+    This is an example python unit test.
+    Make sure the function starts with 'test_'
+    :param name: test string
+    :return: None
+    """
     assert print_hi(name) == 'Hi, {n}'.format(n=name)
 
+
 def print_hi(name):
+    """
+    Example function
+    :param name: test string
+    :return: string output
+    """
     output = 'Hi, {n}'.format(n=name)
     print('Hi, {n}'.format(n=name))
     return output
 
+
 if __name__ == '__main__':
-    print_hi('hello wolrd!')
+    print_hi('hello world!')
