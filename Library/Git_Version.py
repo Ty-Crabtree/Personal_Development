cc1eb2141ac9ebc7f4cd3e9b291b0669e200434c
ty-crabtree <tyler.crabtree@smarsh.com>
2021-06-02 14:41:33 -0700

    Add personal lib

---
+++ Library/Git_Version.py
@@ -0,0 +1,19 @@
+import os
+import logging
+
+
+def init():
+    logging.basicConfig(level=logging.INFO)
+
+
+def version_calculator():
+    init()
+    current_branch = os.popen('git branch --show-current').read()
+    c_count = os.popen(f'git rev-list --count  {current_branch}').read().strip()
+    logging.info(f'Total commits to current branch: {c_count}')
+    d_count = os.popen(f'git rev-list --count  develop').read().strip()
+    logging.info(f'Total commits to develop branch: {d_count}')
+    m_count = os.popen(f'git rev-list --count  master').read().strip()
+    logging.info(f'Total commits to master branch: {m_count}')
+    new_branches = int(c_count) - int(d_count)
+    logging.info(f'New branches added: {new_branches}')
